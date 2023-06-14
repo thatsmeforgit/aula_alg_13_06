4d3c0b841c761f27a6ddd03a0721cb79bd69bd83
thatsmeforgit <igorifpr2015@gmail.com>
2023-06-14 09:48:45 -0300

    main librarie

---
+++ thatsmylibrarie.java
@@ -0,0 +1,51 @@
+// public class thatsmylibrarie {
+//     public static void main(String[] args) {
+       
+//     //metódo do tipo String, retorna String0
+//     public static String getString(){
+//         String st;
+//         st = sc.next();
+        
+//         return st;
+//     }
+
+//     //metódo do tipo inteiro, le um valor com Scanner.
+//     public static int getInt(){
+//         int valor;
+//         valor = sc.nextInt();
+        
+//         return valor;
+//     }
+
+//     //função p/ calcular média
+//     public static float calcularMedia(int a, int b, int c){
+//         float media;
+
+//         media = (a+b+c)/3;
+//         return media;
+//     }
+
+//     //função booleana para verificar se um número é par.
+//     public static boolean ehPar(int a){
+//         boolean par;
+
+//         if(a%2==0){
+//             par = true;
+//         } else {
+//             par = false;
+//         }
+
+//         return par;
+//     }
+
+//     //Função booleana verificando se um número é impar, o processamento dessa função é a negação da função que verifica se um número é par.
+//     public static boolean ehImpar(int a){
+//         boolean impar;
+
+//         impar = !ehPar(a);
+
+//         return impar;
+//     }
+    
+//     }
+// }
