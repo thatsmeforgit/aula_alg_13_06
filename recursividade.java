a0d7bd16d22e39eafe0104b3e80813cb3b02ee93
thatsmeforgit <igorifpr2015@gmail.com>
2023-06-26 14:56:42 -0300

    commit

---
+++ recursividade.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class recursividade {
+    //função fatorial não recursiva
+    public static int fatorial(int n) {
+        int total = 1;
+        
+        for(int i = n;i>1;i--)
+            total *= i;
+        
+        return total;
+    }
+    //--------------------------------------
+
+    //Função fatorial recursiva
+    public static int fatorialRec(int n) {
+        if (n == 0)
+            return 1;
+        
+        return n * fatorialRec(n-1);
+    }
+    //--------------------------------------
+
+    //função fibonacci recursiva
+    public static int fibonacci(int n){
+
+        if (n < 2)
+            return 1;
+        
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    //--------------------------------------
+
+    //função calcula somatoria de uma sequencia
+    //somatorio(5)
+    // 5 = 5 + somatorio(4)
+    // 4 = 4 + somatorio(3)
+    // 3 = 3 + somatorio(2)
+    // 2 = 2 + somatorio(1)
+    // 1 = 1 + somatorio(0)
+
+    public static int somatorio(int n){
+        if(n == 1)
+            return 1;
+
+        return n + somatorio(n-1); 
+    }
+
+    //----------------------------------------------------------------------
+    //saidas
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        //saída função fatorial
+        System.out.printf("%d! = %d\n", n ,fatorialRec(n));
+        
+        //saída função fibonacci com estrutura for
+        for(int i = 0; i<20; i++){
+            System.out.println(fibonacci(i));
+        }
+
+        //saída função somatorio
+        System.out.printf("Somatório de %d = %d", n ,somatorio(n));
+    }
+
+}
--- umadez.java
+++ umadez.java
@@ -1,11 +1,11 @@
 public class umadez{
     
     //função
-        public static void umaDez(){
-            for (int i=1; i<=10; i++){
-                System.out.println(i);
-            }
+    public static void umaDez(){
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
         }
+    }
 
     public static void main(String[] args){
         umaDez();
