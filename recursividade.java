73a76f21969f887428c90d000f7133635566cfe3
thatsmeforgit <igorifpr2015@gmail.com>
2023-06-27 14:02:27 -0300

    commit changes

--- recursividade.java
+++ recursividade.java
@@ -24,20 +24,23 @@ public class recursividade {
     //função fibonacci recursiva
     public static int fibonacci(int n){
 
+        //Indicação de quanto a função deve terminar
         if (n < 2)
             return 1;
         
+        //recursividade
         return fibonacci(n-1) + fibonacci(n-2);
     }
     //--------------------------------------
 
-    //função calcula somatoria de uma sequencia
+    //função calcula somatoria de uma sequencia utilizando recursividade
+    
     //somatorio(5)
-    // 5 = 5 + somatorio(4)
-    // 4 = 4 + somatorio(3)
-    // 3 = 3 + somatorio(2)
-    // 2 = 2 + somatorio(1)
-    // 1 = 1 + somatorio(0)
+    // somatorio(5) = 5 + somatorio(4)
+    // somatorio(4) = 4 + somatorio(3)
+    // somatorio(3) = 3 + somatorio(2)
+    // somatorio(2) = 2 + somatorio(1)
+    // somatorio(1) = 1 + somatorio(0)
 
     public static int somatorio(int n){
         if(n == 1)
@@ -53,15 +56,15 @@ public class recursividade {
         int n = sc.nextInt();
         sc.close();
 
-        //saída função fatorial
+        //saída função recursiva fatorial
         System.out.printf("%d! = %d\n", n ,fatorialRec(n));
         
-        //saída função fibonacci com estrutura for
+        //saída função recursiva fibonacci com estrutura for
         for(int i = 0; i<20; i++){
             System.out.println(fibonacci(i));
         }
 
-        //saída função somatorio
+        //saída função recursiva somatorio
         System.out.printf("Somatório de %d = %d", n ,somatorio(n));
     }
 
